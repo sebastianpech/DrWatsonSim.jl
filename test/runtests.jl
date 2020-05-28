0cb7d5ac822b2b27401be4bc30967745375f5f6d
Sebastian Pech <sebastian.pech@me.com>
2020-05-28 15:24:14 +0200

    Fix number of files from long running calc

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,5 @@
 using Pkg
+pkg"activate ."
 using DrWatsonSim
 const ds = DrWatsonSim
 using Test
@@ -163,7 +164,7 @@ include("helper_functions.jl")
                 file = scriptsdir("long_running_script.jl")
                 cp(joinpath(@__DIR__, "long_running_script.jl"), file)
                 run(`julia $file`)
-                for i in 1:9
+                for i in 1:5
                     folder = datadir("sims","$i")
                     file = datadir("sims","$i","output.bson")
                     @test isfile(file)
